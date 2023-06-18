83b6716a3042a8812fcd42df1a02fc4881a16dc2
ABC10946 <abctech10946@gmail.com>
2023-06-18 13:05:14 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
+from pathlib import Path
+import base64
+import json
+import logging
+app = Flask(__name__)
+
+
+managedPubkeyFile = '/data/managed_pubkey.json'
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+    elif request.method == 'GET':
+        with open(managedPubkeyFile, 'r') as f:
+            fr = f.read()
+            if os.path.getsize(managedPubkeyFile) != 0:
+                try:
+                    ipkeyData = json.loads(fr)
+                    lines = []
+                    for key in ipkeyData.keys():
+                        lines.append(key + ':' + ipkeyData[key])
+
+                    return render_template('index.html', lines = lines)
+                except json.decoder.JSONDecodeError:
+                    return render_template('index.html', message = 'something error occured on json file loading', lines = '')
+            else:
+                return render_template('index.html', lines = '')
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        hostname = request.form['hostname']
+        pubkey = request.form['pubkey']
+        ipkeyData = {}
+
+        with open(managedPubkeyFile, 'r') as f:
+            fr = f.read()
+            if fr != '' and fr != '\n':
+                ipkeyData = json.loads(fr)
+
+
+        with open(managedPubkeyFile, 'w') as f:
+            ipkeyData[hostname] = pubkey
+            ipkeyDataJson = json.dumps(ipkeyData, indent=4)
+            f.write(ipkeyDataJson)
+
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
